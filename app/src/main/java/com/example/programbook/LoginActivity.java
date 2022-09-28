412e984fa7d4bee20f810859e0079cba1a8e9890
CalvinOwen7 <s32200084@student.ubm.ac.id>
2022-09-28 22:38:48 +0700

    update LoginActivity

--- app/src/main/java/com/example/programbook/LoginActivity.java
+++ app/src/main/java/com/example/programbook/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.programbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toast.makeText(LoginActivity.this,"test",Toast.LENGTH_LONG).show();
     }
 }
