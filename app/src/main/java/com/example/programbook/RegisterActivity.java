8edbe5d73c223d63f0b38a466d1b186793caa05b
CalvinOwen7 <s32200084@student.ubm.ac.id>
2022-09-28 23:04:58 +0700

    update RegisterActivity.java

--- app/src/main/java/com/example/programbook/RegisterActivity.java
+++ app/src/main/java/com/example/programbook/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.programbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toast.makeText(RegisterActivity.this,"test",Toast.LENGTH_LONG).show();
     }
 }
